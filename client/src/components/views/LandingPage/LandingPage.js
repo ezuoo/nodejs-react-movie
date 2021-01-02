b9d4240abdf9ff93016e75b7c95b2fb0e2642449
juho0428@naver.com <juho0428@naver.com>
2021-01-03 04:27:18 +0900

    checkout landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,46 +1,66 @@
 import React, {useEffect, useState } from 'react'
 import {API_KEY, API_URL, IMAGE_BASE_URL} from '../../Config';
 import MainImage from './Section/MainImage';
+import GridCards from "../Common/GridCards";
+import {Row} from 'antd';
 
 function LandingPage() {
    
     const [Movies, setMovies] = useState([]);
     const [MainMovieImage, setMainMovieImage] = useState(null);
+    const [CurrentPage, setCurrentPage] = useState(0);
+
+    const fetchMovie = (endPoint) => {
+        fetch(endPoint)
+            .then(response => response.json())
+            .then(response => {
+                setMovies([...Movies, ...response.results]);
+                MainMovieImage === null ? setMainMovieImage(response.results[0]) : setMainMovieImage(MainMovieImage);
+                setCurrentPage(response.page);
+            });
+    }
 
     useEffect(() => {
         const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
-        fetch(endPoint)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            setMovies(...[response.results]);
-            setMainMovieImage(response.results[0]);
-        });
+        fetchMovie(endPoint);
 
     }, []);
 
+    const onClickLoadHandler = () => {
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        fetchMovie(endPoint);
+    }
+
     return (
         <>
-            <div style={{width:'100%', margin:'0'}}>
-                
-            {MainMovieImage &&
-                <MainImage image={`${IMAGE_BASE_URL}w1280/${MainMovieImage.backdrop_path}`} 
-                title={MainMovieImage.title} overview={MainMovieImage.overview}/>
-            }
-                <h2>black</h2>
+            <div style={{width:'100%', margin:'0'}}>   
+                {MainMovieImage &&
+                    <MainImage image={`${IMAGE_BASE_URL}w1280/${MainMovieImage.backdrop_path}`} 
+                    title={MainMovieImage.original_title} overview={MainMovieImage.overview}/>
+                }
             </div>
-
+            <br />    
             <div style={{width: '85%', margin: '1rem auto'}}>
                 <h2>Movie by latest</h2>
-                
                 <hr />
-
-                {/** Movie Grid Card */}
+                <br />
+            
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title} />
+                        </React.Fragment>
+                    ))}
+                </Row>
+            
             </div>
 
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <button>Load More</button>
+                <button onClick={onClickLoadHandler}>Load More</button>
             </div>
         </>
     )
